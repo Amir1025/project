77d93df875c2f858748c25eec98d93792182bebb
Amir <amirhangaytukiev731@gmail.com>
2021-01-04 11:53:09 +0300

    Первый комментарий

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+
+
+
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console. log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя", "Амир");
+// answers[1] = prompt("Как ваша фамилия", "Махлоев");
+// answers[2] = prompt("Сколько вам лет", "31");
+
+// console. log(answers);
+
+
+// const category = "toys";
+
+// console. log(`https://url.com/${category}`);
+
+// const user = "Amir";
+// alert(`Привет, ${user}`);
+
+
+
+
+// let incr = 10,
+// decr = 10;
+
+// incr++;
+// decr--;
+
+
+// console. log(incr);
+// console. log(decr);
+
+// console. log(2+2 === 4);
+
+
+
+// const isCeckend = true,
+//       isClose = false;
+
+//       console. log(isCeckend && isClose);
